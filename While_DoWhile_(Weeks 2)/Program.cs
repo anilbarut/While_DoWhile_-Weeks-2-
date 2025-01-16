163516d88b88b9638519535a1cdd44ad5112584c
anil- <anil-@ANL>
2025-01-16 18:34:10 +0300

    Add project files.

---
+++ While_DoWhile_(Weeks 2)/Program.cs
@@ -0,0 +1,21 @@
+﻿
+{
+    Console.WriteLine("-----1-----");
+    int sayac = 0;
+    while (sayac <= 10)
+    {
+        Console.WriteLine("Ben bir Patika'lıyım");
+        sayac++;
+    }
+}
+
+Console.WriteLine("------2------");
+{
+    int sayac = 0;
+    do
+    {
+        Console.WriteLine("Ben bir Patika'lıyım");
+        sayac++;
+    }
+    while (sayac <= 10);
+}
